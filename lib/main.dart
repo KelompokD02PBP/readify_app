696989902a1d4faa44ada935256cab61824d2422
venedictchen <venedictchen@gmail.com>
2023-12-03 15:11:18 +0700

    feat: ui profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:readify_app/screens/login.dart';
+import 'package:readify_app/screens/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const ProfilePage(),
     );
   }
 }
