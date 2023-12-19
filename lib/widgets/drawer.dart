e2e8b409d07c219415334488487dacffb16758b7
KenKomKom <kenichikomala@gmail.com>
2023-12-19 08:33:53 +0700

    a

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:readify_app/screens/HomePage.dart';
-import 'package:readify_app/screens/likes_page2.dart';
 import 'package:readify_app/screens/profile.dart';
+import 'package:readify_app/screens/likes_page2.dart';
 import '../classes/pbp_django_auth.dart';
 import '../screens/login.dart';
 
 class EndDrawer extends StatelessWidget {
-
   const EndDrawer({super.key});
 
   @override
@@ -17,8 +16,8 @@ class EndDrawer extends StatelessWidget {
       child: ListView(
         children: [
           const DrawerHeader(
-            decoration: BoxDecoration(
-               color: Colors.amberAccent,
+              decoration: BoxDecoration(
+              color: Color.fromARGB(255, 56, 30, 103),
             ),
             child: Column(
               children: [
@@ -28,28 +27,18 @@ class EndDrawer extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
-                    color: Colors.black87,
+                    color: Colors.white,
                   ),
                 ),
                 Padding(padding: EdgeInsets.all(10)),
-                Text(
-                  "Review Buku!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black87, fontSize: 15),
-                ),
+                Text("Review Buku!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color:Colors.white, fontSize: 15),
+                    ),
               ],
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.account_circle_outlined),
-            title: const Text('Profile'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProfilePage()),
-              );
-            },
-          ),
+          
           ListTile(
             leading: const Icon(Icons.home_outlined),
             title: const Text('Halaman Utama'),
@@ -60,7 +49,17 @@ class EndDrawer extends StatelessWidget {
                   MaterialPageRoute(
                     builder: (context) => const MyHomePage(),
                   ));
-            },
+              },
+          ),
+          ListTile(
+              leading: const Icon(Icons.shopping_basket),
+              title: const Text('Profile'),
+              onTap: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfilePage()),
+                  );
+              },
           ),
           ListTile(
             leading: const Icon(Icons.favorite_border_sharp),
@@ -103,25 +102,9 @@ class EndDrawer extends StatelessWidget {
 
               }
             },
-          ),
+          )
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
